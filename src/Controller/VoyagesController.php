674ae225ce66eb7e64fa02802a4a8a52dbcf0ee5
taizxyz <raphaelpv@protonmail.com>
2026-02-26 21:51:33 +0100

    ORM, affichage des informations provenant de la bdd

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class VoyagesController extends AbstractController
+class VoyagesController extends AbstractController
 {
     #[Route('/voyages', name: 'app_voyages')]
-    public function index(): Response
+    public function index(VisiteRepository $repository): Response
     {
+        // recup données table visite
+        $visites = $repository->findAll();
+
+        // envoie des données à la vue
         return $this->render('voyages/index.html.twig', [
-            'controller_name' => 'VoyagesController',
+            'visites' => $visites,
         ]);
     }
 }
